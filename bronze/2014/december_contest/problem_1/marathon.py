576cbfbed750a588761fd592f2bb7f0c06e3dc21
ilianiv <ilian.iv@gmail.com>
2015-12-10 22:00:05 +0200

    fix marathon and crosswords use input and output files

--- bronze/2014/december_contest/problem_1/marathon.py
+++ bronze/2014/december_contest/problem_1/marathon.py
@@ -1,14 +1,14 @@
-import sys
+f = open('marathon.in', 'r')
 
-n = int(sys.stdin.readline())
-p2 = list(map(int, sys.stdin.readline().split(' ')))
-p1 = list(map(int, sys.stdin.readline().split(' ')))
+n = int(f.readline())
+p2 = list(map(int, f.readline().split(' ')))
+p1 = list(map(int, f.readline().split(' ')))
 max = 0
 maxi = 0
 path = abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
 
 for x in range(2, n):
-    p0 = list(map(int, sys.stdin.readline().split(' ')))
+    p0 = list(map(int, f.readline().split(' ')))
     diff21 = abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
     diff01 = abs(p0[0] - p1[0]) + abs(p0[1] - p1[1])
     diff02 = abs(p0[0] - p2[0]) + abs(p0[1] - p2[1])
@@ -20,4 +20,9 @@ for x in range(2, n):
     p1 = p0
     path += diff01
 
-print(path - max)
+f.close()
+
+f = open('marathon.out', 'w')
+f.write("{0}\n".format(path - max))
+f.close()
+
--- bronze/2014/december_contest/problem_2/crosswords.py
+++ bronze/2014/december_contest/problem_2/crosswords.py
@@ -1,7 +1,10 @@
-import sys
+f = open('crosswords.in', 'r')
 
-[n, m] = list(map(int, sys.stdin.readline().split(' ')))
-field = [row.strip() for row in sys.stdin]
+[n, m] = list(map(int, f.readline().split(' ')))
+field = [row.strip() for row in f]
+
+f.close()
+
 clues = list()
 
 for i in range(n):
@@ -13,7 +16,10 @@ for i in range(n):
                 clues.append([i + 1, j + 1])
 
 count = len(clues)
-print(count)
+
+f = open('crosswords.out', 'w')
+f.write("{0}\n".format(count))
 
 for i in range(count):
-    print("{0} {1}".format(clues[i][0], clues[i][1]))
+    f.write("{0} {1}\n".format(clues[i][0], clues[i][1]))
+f.close()
