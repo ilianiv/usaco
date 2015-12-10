d8c00e1dddfadaf383bb30d0d51478064e4d264a
Iliyan Ivanov <iivanov@fair-point.com>
2015-12-10 17:49:51 +0200

    add solution for crosswords

---
+++ bronze/2014/december_contest/problem_2/crosswords.py
@@ -0,0 +1,19 @@
+import sys
+
+[n, m] = list(map(int, sys.stdin.readline().split(' ')))
+field = [row.strip() for row in sys.stdin]
+clues = list()
+
+for i in range(n):
+    for j in range(m):
+        if  field[i][j] == '.':
+            if (i == 0 or field[i-1][j] == '#') and i < n-2 and field[i+1][j] == '.' and field[i+2][j] == '.':
+                clues.append([i+1,j+1])
+            elif (j == 0 or field[i][j-1] == '#') and j < m-2 and field[i][j+1] == '.' and field[i][j+2] == '.':
+                clues.append([i+1,j+1])
+
+count = len(clues)
+print(count)
+
+for i in range(count):
+    print("{0} {1}".format(clues[i][0], clues[i][1]))
